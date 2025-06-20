1378569bc0975859ee294a64fb4555ace9f4f212
fuugafuu <fuga2013703@gmail.com>
2025-06-20 18:47:12 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// カメラ起動
+navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+  video.srcObject = stream;
+});
+
+// 物体・顔モデルの読み込み
+Promise.all([
+  cocoSsd.load(),
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models')
+]).then(([cocoModel]) => {
+  model = cocoModel;
+  detect();
+});
+
+function detect() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  async function frame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const predictions = await model.detect(video);
+    predictions.forEach(pred => {
+      ctx.strokeStyle = '#00FF00';
+      ctx.lineWidth = 2;
+      ctx.strokeRect(...pred.bbox);
+      ctx.fillStyle = '#00FF00';
+      ctx.fillText(pred.class, pred.bbox[0], pred.bbox[1] > 10 ? pred.bbox[1] - 5 : 10);
+    });
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+    detections.forEach(det => {
+      const { x, y, width, height } = det.box;
+      ctx.strokeStyle = 'red';
+      ctx.strokeRect(x, y, width, height);
+      ctx.fillStyle = 'red';
+      ctx.fillText('Face', x, y > 10 ? y - 5 : 10);
+    });
+
+    requestAnimationFrame(frame);
+  }
+
+  frame();
+}
